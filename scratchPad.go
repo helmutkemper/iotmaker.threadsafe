728d8025ce673cb0bd606ea17efb9ea1e4936973
Helmut Kemper <helmut.kemper@gmail.com>
2021-06-20 20:46:26 -0300

    refactoring

--- scratchPad.go
+++ scratchPad.go
@@ -1,7 +1,7 @@
 package iotmaker_threadsafe
 
 import (
-	iotmaker_platform_IDraw "github.com/helmutkemper/iotmaker.platform.IDraw"
+	iotmaker_platform_IDraw "github.com/helmutkemper/iotmaker.santa_isabel_theater.platform.IDraw"
 	"sync"
 )
 
